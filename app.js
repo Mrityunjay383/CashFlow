a81c8c94d5dd1e2631eb5f62043cbe945882be06
Mrityunjay <mrityunjayvyasmhl@gmail.com>
2022-04-04 19:27:10 +0530

    Deployed Ready

--- app.js
+++ app.js
@@ -42,6 +42,6 @@ app.post("/addTrans", async (req, res) => {
   res.redirect("/");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running");
 })
